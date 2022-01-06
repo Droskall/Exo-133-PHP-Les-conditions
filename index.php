06c7fc15428359c474ee23103623319b3a4959ec
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2022-01-06 16:23:28 +0100

    fini

--- index.php
+++ index.php
@@ -4,33 +4,34 @@
 $a = 50;
 $b = 10;
 
-       >    {
+   if( $a > $b){
     echo "Hello World!";
 }
 
 //Deuxieme ligne
-    {
+    if ($a !== $b){
     echo "<br><br>Hello World2";
 }
 
 //Troisieme ligne
-($a==b) {
+if($a==$b) {
     echo "<br><br>Oui";
 }
 
-{
+else{
     echo "<br><br>Non";
 }
 
 
 //Quatrieme ligne
 echo"<br><br>";
- ($a == $b) {
+ if($a == $b) {
     echo "1";
-}  ($a > $b) {
+}
+elseif($a > $b) {
     echo "2";
 }
-{
+else{
     echo "3";
 }
 
@@ -38,10 +39,14 @@ echo"<br><br>";
 //Cinquieme ligne
 echo"<br><br>";
 
- ($color) {
-    "red":
-    echo "Hello";
-   "green":
+ if($color = "red") {
+     echo "Hello";
+ }
+
+ elseif( $color ="green"){
     echo "Welcome";
 }
+ else{
+     echo "rien";
+ }
 
